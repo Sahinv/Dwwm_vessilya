bcff6321775998a90d6864ef402f5aafe665bc75
Sahinv <vessilyadu59760@gmail.com>
2020-05-11 16:59:35 +0200

    maj

--- Dossier_Wordpress/Plug-in/HelloWorld_essaie/HelloWorld.php
+++ Dossier_Wordpress/Plug-in/HelloWorld_essaie/HelloWorld.php
@@ -1,29 +1,18 @@
-<?php
-// donne l’accès à la base de données
-global $wpdb;
-
-/*
-Plugin Name: Hello World
-Description: Ecrit Hello World en haut de la page
+<?php /* Plugin Name: Hello World
+Description: ecrit Hello World
 Author: Martine
-Version: 1.0
- */
-echo "<h1>Hello world</h1>";
-
+Version: 1.0 */
 class HelloWorld_Plugin
 {
     public function __construct()
     {
         include_once plugin_dir_path(__FILE__) . '/helloClass.php';
-        // On instantie la class ( créer )
         new HelloClass();
-
-        // Installe les commentaires dans la BDD
+        //Installe
         register_activation_hook(__FILE__, array('helloclass', 'install'));
-
-        // Supprime les commentaires dans la BDD
+        //desinstalle
         register_deactivation_hook(__FILE__, array('helloclass', 'uninstall'));
     }
 }
-// Création de l'objet 
 new HelloWorld_Plugin();
+//rtgrgr
